2fb07a5710bb3f2bc4432a3a1edaa68be9ad6c5e
alamshafil <alamshafil@pm.me>
2023-06-05 22:51:20 -0400

    client-server: Format JS code

--- client-server/main.js
+++ client-server/main.js
@@ -10,7 +10,7 @@ import ArcadePlayHistory from './models/playHistory.js'
 import Arcade from './models/arcade.js'
 
 // ZMQ, RPC, WS imports
-import zmq from 'zeromq' 
+import zmq from 'zeromq'
 import RpcClient from 'bitcoind-rpc'
 import { WebSocketServer } from 'ws'
 
@@ -37,7 +37,7 @@ var rpc_config = {
 console.info("[MongoDB] Starting memory server...")
 const mongod = await MongoMemoryServer.create({
     instance: {
-      port: MONGODB_PORT
+        port: MONGODB_PORT
     }
 });
 
@@ -49,11 +49,11 @@ const wss = new WebSocketServer({ port: WEBSOCKET_PORT })
 console.info(`[WS] Websocket server is running at port ${WEBSOCKET_PORT}...`)
 
 // Set up RPC
-var rpc = new RpcClient(rpc_config); 
+var rpc = new RpcClient(rpc_config);
 
 // Check if RPC is up
-rpc.help(err => { 
-    if(err) console.error("[RPC] " + err)
+rpc.help(err => {
+    if (err) console.error("[RPC] " + err)
     else console.info("[RPC] Connected to Doge RPC...")
 })
 
@@ -65,20 +65,20 @@ console.info("[ZMQ] Connected to ZMQ...")
 
 // MongoDB
 // Connect to DB
-mongoose.connect(uri+'arcade', {useNewUrlParser: true});
+mongoose.connect(uri + 'arcade', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Connection error: "));
 
 db.once("open", function () {
-  console.info("[MongoDB] Connected to DB...");
+    console.info("[MongoDB] Connected to DB...");
 });
 
 // ZMQ
 sock.on('message', (topic, message) => {
     rpc.getTransaction(message.toString('hex'), (err, resp) => {
-        if(err) console.error("[RPC] " + err)
+        if (err) console.error("[RPC] " + err)
         var tx = resp.result
         var hash = tx.txid
         var from = tx.details[0].address
@@ -90,7 +90,7 @@ sock.on('message', (topic, message) => {
         wss.clients.forEach(function each(ws) {
             if (ws.isAlive === false) return ws.terminate()
             console.info("[WS] Sending TX to all websocket clients...")
-            var data = {action: "tx", data: {from, to, value, hash, timestamp}}
+            var data = { action: "tx", data: { from, to, value, hash, timestamp } }
             ws.send(JSON.stringify(data))
         })
     })
@@ -99,19 +99,19 @@ sock.on('message', (topic, message) => {
 // WS
 wss.on("connection", (ws, req) => {
     console.info(`[WS] ${req.socket.remoteAddress} has connected...`);
-    ws.on("message", data => {      
+    ws.on("message", data => {
         try {
             var message = JSON.parse(data)
         } catch (e) {
             console.error("[WS] Invalid message was sent! Raw data: " + data)
             return;
         }
-        if(message.action != null && message.data != null) {
+        if (message.action != null && message.data != null) {
             var info = message.data
             switch (message.action) {
                 case "paid":
                     console.info(`[WS] Got ${message.action} where ${info.from} sent ${info.value} DOGE to machine '${info.arcade_name}'`)
-                    const arcadeHistory = new ArcadeHistory({from: info.from, value: info.value, arcade_name: info.arcade_name, arcade_address: info.arcade_address, tx: info.tx, timestamp: info.timestamp})
+                    const arcadeHistory = new ArcadeHistory({ from: info.from, value: info.value, arcade_name: info.arcade_name, arcade_address: info.arcade_address, tx: info.tx, timestamp: info.timestamp })
                     try {
                         arcadeHistory.save();
                     } catch (error) {
@@ -119,7 +119,7 @@ wss.on("connection", (ws, req) => {
                     }
                     break;
                 case "play_game":
-                    const playHistory = new ArcadePlayHistory({from: info.from, value: info.value, timestamp: info.timestamp, arcade_name: info.arcade_name, arcade_address: info.arcade_address})
+                    const playHistory = new ArcadePlayHistory({ from: info.from, value: info.value, timestamp: info.timestamp, arcade_name: info.arcade_name, arcade_address: info.arcade_address })
                     try {
                         playHistory.save();
                     } catch (error) {
@@ -128,38 +128,38 @@ wss.on("connection", (ws, req) => {
                     break;
                 case "leave":
                     // Add status to DB
-                    console.info(`[WS] Arcade machine '${info.arcade_name} has left.'`)   
-                    Arcade.findOneAndUpdate({address: info.arcade_address}, {
+                    console.info(`[WS] Arcade machine '${info.arcade_name} has left.'`)
+                    Arcade.findOneAndUpdate({ address: info.arcade_address }, {
                         status: {
                             online: false,
                             timestamp: info.timestamp
                         }
                     }, {}, function (err, docs) {
-                        if(err) console.error(`[DB] Failed to update status for '${info.arcade_name}.' Error: ${err}`)
+                        if (err) console.error(`[DB] Failed to update status for '${info.arcade_name}.' Error: ${err}`)
                         else console.info(`[DB] Saved machine '${info.arcade_name}' status to offline at ${info.timestamp}`)
                     });
                     break;
                 case "join":
                     console.info(`[WS] Arcade machine '${info.arcade_name}' has joined.`)
-                    Arcade.find({ address: info.arcade_address}, function (err, machines) {
+                    Arcade.find({ address: info.arcade_address }, function (err, machines) {
                         // Send cost data to machine
                         console.info(`[WS] Sending infomation from DB to machine '${info.arcade_name}'`)
                         if (err || machines.length == 0) {
-                            var json = {action: "cost", data: {arcade_name: info.arcade_name, arcade_address: info.arcade_address, cost: "Error", name: "Error"}}
+                            var json = { action: "cost", data: { arcade_name: info.arcade_name, arcade_address: info.arcade_address, cost: "Error", name: "Error" } }
                             ws.send(JSON.stringify(json))
                         } else {
-                            var json = {action: "cost", data: {arcade_name: info.arcade_name, arcade_address: info.arcade_address, cost: machines[0].cost, name: machines[0].name}}
+                            var json = { action: "cost", data: { arcade_name: info.arcade_name, arcade_address: info.arcade_address, cost: machines[0].cost, name: machines[0].name } }
                             ws.send(JSON.stringify(json))
                         }
                     });
                     // Add status to DB
-                    Arcade.findOneAndUpdate({address: info.arcade_address}, {
+                    Arcade.findOneAndUpdate({ address: info.arcade_address }, {
                         status: {
                             online: true,
                             timestamp: info.timestamp
                         }
                     }, {}, function (err, docs) {
-                        if(err) console.error(`[DB] Failed to update status for '${info.arcade_name}.' Error: ${err}`)
+                        if (err) console.error(`[DB] Failed to update status for '${info.arcade_name}.' Error: ${err}`)
                         else console.info(`[DB] Saved machine '${info.arcade_name}' status to online at ${info.timestamp}`)
                     });
                     break;
